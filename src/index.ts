0be5c7a0513ba2892588261b673be0e207d82447
Megana <megana15081990@yandex.ru>
2024-09-21 23:16:21 +0700

    клас EventEmitter

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
+import { CardsData } from './components/base/CardsData';
+import { FormData } from './components/base/FormData';
+import { BasketData } from './components/base/BasketData';
+import { EventEmitter } from './components/base/events';
 import './scss/styles.scss';
+// import { BasketData } from './components/common/BasketData';
+// import { CardsData } from './components/common/CardsData'
+// import { FormData } from './components/common/FormData';
 
+const events = new EventEmitter();
+
+const basketData = new BasketData(events);
+const cardsdData = new CardsData(events);
+const formData = new FormData(events);
