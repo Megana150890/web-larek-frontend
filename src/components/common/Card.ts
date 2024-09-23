27418fe9f8dea4352ccb663554c3cc203e5ce4bb
Megana <megana15081990@yandex.ru>
2024-09-23 16:47:54 +0700

    клас Card

--- src/components/common/Card.ts
+++ src/components/common/Card.ts
@@ -13,17 +13,53 @@ export class Card extends Component<ICard> {
     protected _image:HTMLImageElement;
 	protected _title: HTMLElement;
 	protected _category: HTMLElement;
-    protected button: HTMLButtonElement;
+    protected _button: HTMLButtonElement;
 
    constructor( protected blockName: string, container: HTMLElement, actions?: ICardActions) {
     super(container);
-    
+
     this._description =  container.querySelector('.card__text');
     this._price = ensureElement<HTMLElement>('.card__price', container);
     this._image = container.querySelector('.card__image');
     this._title = ensureElement<HTMLElement>('.card__title', container);
     this._category = container.querySelector('.card__category')
-    this.button = container.querySelector('.card__button')
+    this._button = container.querySelector('.card__button')
+
+    if (actions?.onClick) {
+        if (this._button) {
+            this._button.addEventListener('click', actions.onClick);
+        } else {
+            container.addEventListener('click', actions.onClick);
+        }
    }
+}
+
+set description(value: string) {
+    this.setText(this._description, value)
+}
+
+set price(value:string) {
+    this.setText(this._price, value)
+}
+
+set image(value: string) {
+    this.setImage(this._image, value)
+}
+
+set category(value: string) {
+    this.setText(this._category, value)
+}
+
+set title(value: string) {
+    this.setText(this._title, value);
+}
   
+set button(element:HTMLButtonElement) {
+    this._button = element;
+}
+
+get button():HTMLButtonElement {
+    return this.button
+}
+
 }
